0d5873f7c66f3751e274006e9e4e8678b03abff0
User <User@DESKTOP-6N6EHH6>
2020-11-21 22:57:04 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan58/tambah/kurang/Bilangan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan58.tambah.kurang;
+
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi: Tambah Kurang
+ */
+public class Bilangan {
+    private int x;
+    private int y;
+
+    public Bilangan() {
+        this.x = 3;
+        this.y = 4;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+}

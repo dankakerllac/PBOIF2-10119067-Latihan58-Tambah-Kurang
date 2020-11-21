2c9a04f5147c5c1eb9b402fa19dcde370b506254
User <User@DESKTOP-6N6EHH6>
2020-11-21 22:55:46 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan58/tambah/kurang/PBOIF210119067Latihan58TambahKurang.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan58.tambah.kurang;
+
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi: Tambah Kurang
+ */
+public class PBOIF210119067Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan jumlah = new JumlahBilangan();
+        jumlah.tampilHasilJumlah();
+        SelisihBilangan selisih = new SelisihBilangan();
+        selisih.tampilSelisih();
+    }
+
+    private static class SelisihBilangan {
+
+        public SelisihBilangan() {
+        }
+
+        private void tampilSelisih() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+    
+}
